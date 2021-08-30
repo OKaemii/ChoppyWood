6ea451678a832d4119cc8dcdd47ab9e274dda443
Okamii <2314120h@student.gla.ac.uk>
2021-08-30 18:37:01 +0100

    Added display screen

--- Timber.cpp
+++ Timber.cpp
@@ -1,8 +1,41 @@
-#include <iostream>
+//#include <iostream>
+#include <SFML/Graphics.hpp>
+
+// make code easier to read
+using namespace sf;
 
 // this is where game starts
 int main()
 {
-    std::cout << "Hello World!\n";
+    // create a video mode object of 1920x1080 pixels
+    VideoMode vm(1920, 1080);
+
+    // create and open a window for the game
+    RenderWindow window(vm, "Timber!", Style::Default);
+
+    while (window.isOpen())
+    {
+        /*
+        * Handle Player Input
+        */
+        if (Keyboard::isKeyPressed(Keyboard::Escape))
+        {
+            window.close();
+        }
+
+        /*
+        * Update the Scene
+        */
+        // nothing to do for now
+
+        /*
+        * Draw the Scene
+        */
+        // draw a new scene
+        window.clear();
+        window.display();
+    }
+
     return 0;
+
 }
