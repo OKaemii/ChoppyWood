ca64e4c6e8463f733a52777f1fd40062045cf174
Okamii <fahadhafiz1998@gmail.com>
2021-08-31 01:16:33 +0100

    Added cloud sprites

--- Timber.cpp
+++ Timber.cpp
@@ -17,26 +17,50 @@ int main()
     Texture textureBackground;
     Texture textureTree;
     Texture textureBee;
+    Texture textureCloud;
 
     // load background img into GPU
     textureBackground.loadFromFile("graphics/background.png");
     textureTree.loadFromFile("graphics/tree.png");
     textureBee.loadFromFile("graphics/bee.png");
+    textureCloud.loadFromFile("graphics/cloud.png");
 
     // init background, and tree as sprite for placement
     Sprite spriteBackground;
     Sprite spriteTree;
     Sprite spriteBee;
+
+    Sprite spriteCloud1;
+    Sprite spriteCloud2;
+    Sprite spriteCloud3;
+    
     
     // link sprite and graphics
     spriteBackground.setTexture(textureBackground);
     spriteTree.setTexture(textureTree);
     spriteBee.setTexture(textureBee);
 
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud3.setTexture(textureCloud);
+
     // set position of background and sprite relative to viewscreen
     spriteBackground.setPosition(0,0);
     spriteTree.setPosition(810, 0);
     spriteBee.setPosition(0, 800);
+
+    spriteCloud1.setPosition(0, 0);
+    spriteCloud2.setPosition(0, 250);
+    spriteCloud3.setPosition(0, 500);
+
+    // clouds current on screen?
+    bool cloud1Active = false;
+    bool cloud2Active = false;
+    bool cloud3Active = false;
+    // cloud stats
+    float cloud1Speed = 0.0f;
+    float cloud2Speed = 0.0f;
+    float cloud3speed = 0.0f;
 
     // is bee currently mobile?
     bool beeAcive = false;
